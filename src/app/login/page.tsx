95fbb04d9db70a708f4741614c41362bce70496d
FG420 <facchinatog97@gmail.com>
2023-10-29 18:06:15 +0100

    style login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,15 +36,28 @@ export default function LoginPage() {
 
 
     return (
-        <div>
-            <h1>Login</h1>
-            <br />
-            <br />
-            <label  htmlFor="username">Username</label>
-            <input className='text-black' type="text" name="" id="" value={admin.username} onChange={(e) => setAdmin({...admin, username: e.target.value})}/>
-            <label htmlFor="password">Password</label>
-            <input className='text-black' type="password" name="" id="" value={admin.password} onChange={(e) => setAdmin({...admin, password: e.target.value})}/>
-            <button className='bg-white text-black' onClick={onLogin}>{buttonDisabled? 'Fill the fields' : 'Login'}</button>
+        <div className='flex content-center'>
+            <div className='w-2/5  bg-gray-700  p-5 '>
+                <h1 className='flex justify-center text-4xl font-semibold py-10'>Login</h1>
+                <div className=' py-5'>
+                    <label className='flex justify-center font-medium' htmlFor="username">Username</label>
+                    <div className=' flex justify-center'>
+                        <input className='  text-black' type="text" name="" id="" value={admin.username} onChange={(e) => setAdmin({ ...admin, username: e.target.value })} />
+                    </div>
+                </div>
+                <div className=' py-5'>
+                    <label className='flex justify-center font-medium' htmlFor="password">Password</label>
+                    <div className=' flex justify-center'>
+                        <input className='flex text-black ' type="password" name="" id="" value={admin.password} onChange={(e) => setAdmin({ ...admin, password: e.target.value })} />
+                    </div>
+                </div>
+                <div className=' flex justify-center py-5'>
+                    <button className=' font-bold border-2 border-orange-500 rounded-full bg-orange-600 p-2 w-40 text-white hover:text-white hover:bg-orange-500 shadow hover:shadow-gray-400' onClick={onLogin}>{buttonDisabled ? 'Fill the fields' : 'Login'}</button>
+                </div>
+            </div>
+            <div className= ' bg-gradient-radial from-red-600 via-orange-400 to-yellow-300 flex justify-center w-full p-10'>
+                <img className='' src="vercel.svg" alt="" />
+            </div>
         </div>
     )
 }
