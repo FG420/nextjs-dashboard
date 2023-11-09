06e2f37498dee15385818ec12fc6ef7dc4b4fb3e
FG420 <facchinatog97@gmail.com>
2023-11-09 10:30:32 +0100

    flex justify-evenly

--- src/components/courses/vista/page.tsx
+++ src/components/courses/vista/page.tsx
@@ -23,7 +23,7 @@ export default function VistaCoursesPage() {
                 startingDate: Date | string,
                 endingDate: Date | string,
                 minRequired: number,
-                id: ObjectId    
+                id: ObjectId
             }) => {
                 const course: tCourse = {
                     name: i.name,
@@ -35,9 +35,9 @@ export default function VistaCoursesPage() {
                 return course
             }))
 
-    // const getCourseId = async () => {
-        
-    // }
+            // const getCourseId = async () => {
+
+            // }
 
         } catch (error: any) {
             console.log(error.message)
@@ -55,12 +55,15 @@ export default function VistaCoursesPage() {
             {/* <button className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6 m-4  rounded border-2 border-gray-500'>Add Course</button> */}
             {courses.map((course, index) => (
                 <div key={index} className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6 m-4 bg-gray-700 border-2 border-gray-500 rounded-lg '>
-                    <h1 className="text-white font-bold">{course.name.toUpperCase()}</h1>
-                    <p className="text-white">Starting Date: {new Date(course.startingDate).toLocaleString()}</p>
-                    <p className="text-white">Ending Date: {new Date(course.endingDate).toLocaleString()}</p>
-                    <p className="text-white">Partecipants Required: {course.minRequired} min.</p>
+                    <h1 className="text-white font-bold flex justify-evenly">{course.name.toUpperCase()}</h1>
+                    <p className="text-white flex justify-evenly">Starting Date: {new Date(course.startingDate).toLocaleString()}</p>
+                    <p className="text-white flex justify-evenly">Ending Date: {new Date(course.endingDate).toLocaleString()}</p>
+                    <p className="text-white flex justify-evenly">Partecipants Required: {course.minRequired} min.</p>
                     {/* <p className="text-white">id: {course.id}</p> */}
-                    <button className=' font-bold bg-orange-600 text-white rounded-full p-1 mt-5 w-24 hover:bg-orange-400 '><Link href={`/${course.id}`}>Details</Link></button>
+                    <div className='flex justify-evenly'>
+                        <button className=' font-bold bg-orange-600 text-white rounded-full p-1 mt-5 w-24 hover:bg-orange-400 '><Link href={`/${course.id}`}>Details</Link></button>
+                        <button className=' font-bold bg-orange-600 text-white rounded-full p-1 mt-5 w-24 hover:bg-orange-400 '>Delete</button>
+                    </div>
                 </div>
             ))}
         </div>
